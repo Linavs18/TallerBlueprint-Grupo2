8ae0fdc47b192c6f3d54a136a811806d50cbbfcd
Sebastian Rodriguez Cruz <sr1290853@gmail.com>
2025-10-16 11:47:39 -0500

    Rutas
    (FEAT) Se ajustaron las rutas

--- project-blue/routes/web.php
+++ project-blue/routes/web.php
@@ -1,45 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProjectUserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('users', UserController::class)->except(['show']);
 
-Route::resource('users', App\Http\Controllers\UserController::class)->except('show');
+Route::resource('projects', ProjectController::class);
 
-Route::resource('projects', App\Http\Controllers\ProjectController::class)->except('show');
+Route::resource('tasks', TaskController::class);
 
-Route::resource('tasks', App\Http\Controllers\TaskController::class)->except('show');
-
-Route::resource('project-users', App\Http\Controllers\ProjectUserController::class)->except('show');
-
-
-Route::resource('users', App\Http\Controllers\UserController::class)->except('show');
-
-Route::resource('projects', App\Http\Controllers\ProjectController::class)->except('show');
-
-Route::resource('tasks', App\Http\Controllers\TaskController::class)->except('show');
-
-Route::resource('project-users', App\Http\Controllers\ProjectUserController::class)->except('show');
-
-
-Route::resource('users', App\Http\Controllers\UserController::class)->except('create', 'edit');
-
-Route::resource('projects', App\Http\Controllers\ProjectController::class);
-
-Route::resource('tasks', App\Http\Controllers\TaskController::class);
-
-Route::resource('project-users', App\Http\Controllers\ProjectUserController::class)->except('create', 'edit', 'show');
+Route::resource('project-users', ProjectUserController::class)->except(['show']);
